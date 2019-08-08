36b50ca4de6eed31dcd480e3a5762d51f13d54ec
COLIN\User <punkmuppet@gmail.com>
2019-08-08 01:00:19 +0100

    Added a splash screen and 5 splash images.
    The splash images are displayed randomly.

---
+++ Concentration/SplashScreen.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Concentration
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        Random randomNumber = new Random();
+
+
+        public SplashScreen()
+        {
+            int rInt = randomNumber.Next(1, 6);
+            InitializeComponent();
+
+            //RandomImage.Source = new BitmapImage(new Uri((@"/images/ConcentrationLogo5.png"), UriKind.RelativeOrAbsolute));
+
+            RandomImage.Source = new BitmapImage(new Uri((@"/images/ConcentrationLogo" + rInt + ".png"), UriKind.RelativeOrAbsolute));
+        }
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow game = new MainWindow();
+            game.Show();
+            this.Close();
+        }
+    }
+}
